bcc9aa201fe1e53ec8bc24ead1a4d38f8ded4668
Andrew Moynahan <andrewmoynahan@ajm-mac.local>
2019-06-20 13:31:27 -0500

    updated for my own stuff

--- IBM_Cloud/iks/main.tf
+++ IBM_Cloud/iks/main.tf
@@ -1,6 +1,9 @@
-
+provider "ibm" {
+    #softlayer_username = "${var.sl_username}"
+    #softlayer_api_key = "${var.sl_api_key}"
+}
 module "cluster" {
-  source  = "git::https://github.com/IBM-CAMHub-Open/starterlibrary.git?ref=2.2//IBM_Cloud/modules/ibm_cloud_kubernetes_cluster"
+  source  = "git::https://github.com/amblerman/starterlibrary/tree/master/IBM_Cloud/modules/ibm_cloud_kubernetes_cluster"
   org = "${var.org}"
   space = "${var.space}" 
   cluster_name = "${var.cluster_name}" 
@@ -17,7 +20,7 @@ module "cluster" {
 }
 
 module "tiller" {
-  source  = "git::https://github.com/IBM-CAMHub-Open/starterlibrary.git?ref=2.2//IBM_Cloud/modules/helm_tiller"
+  source  = "git::https://github.com/amblerman/starterlibrary/tree/master/IBM_Cloud/helm-tiller"
   deploy_tiller = "${var.deploy_tiller}"
   cluster_name = "${var.cluster_name}"
   cluster_config = "${module.cluster.cluster_config}"
